e5e1d860da26de43377d1cd9edca7f1c8c3549d0
Sang T. Truong <sttruong@stanford.edu>
2021-07-04 15:16:36 -0400

    Delete pythondowload.py

--- helpers/pythondowload.py
+++
@@ -1,8 +0,0 @@
-##Python 3
-import urllib.request
-url = "https://github.com/sangttruong/incomevis/raw/gh-pages/src/input/ipums-cps-lite2.gz"
-print ("download start!")
-filename, headers = urllib.request.urlretrieve(url, filename="ipums-cps-lite2.gz")
-print ("download complete!")
-print ("download file location: ", filename)
-print ("download headers: ", headers)
