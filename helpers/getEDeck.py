6d17d8b20b2dacf9edba18de375004a830193d15
Sang T. Truong <sttruong@stanford.edu>
2021-07-04 20:45:42 -0400

    rename helpers to utils

--- helpers/getEDeck.py
+++
@@ -1,7 +0,0 @@
-def getEDeck ():
-  eDeck = ['e1A','e2A','e3A','e4A','e5A','e6A','e7A','e8A','e9A','e10A','e11A','e12A','e13A',
-           'e1B','e2B','e3B','e4B','e5B','e6B','e7B','e8B','e9B','e10B','e11B','e12B','e13B',
-           'e1C','e2C','e3C','e4C','e5C','e6C','e7C','e8C','e9C','e10C','e11C','e12C','e13C',
-           'e1D','e2D','e3D','e4D','e5D','e6D','e7D','e8D','e9D','e10D','e11D','e12D','e13D']
-
-  return eDeck
